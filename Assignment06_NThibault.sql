c0a05c737a12093ffd02a922efb56766d0283a9f
Dilong-paradoxus <32314600+Dilong-paradoxus@users.noreply.github.com>
2025-05-27 21:45:37 -0700

    Update Assignment06_NThibault.sql
    Final (hopefully) submission version

--- Assignment06_NThibault.sql
+++ Assignment06_NThibault.sql
@@ -178,51 +178,94 @@ print
 
 CREATE
 VIEW 
-CategoriesView
+vCategories
+WITH SCHEMABINDING
 AS
 SELECT
 	CategoryID, CategoryName
-		FROM Categories;
+		FROM dbo.Categories;
 go
 
 CREATE
 VIEW
-EmployeesView AS
+vEmployees
+WITH SCHEMABINDING
+AS
 SELECT
 EmployeeID,EmployeeFirstName,EmployeeLastName,ManagerID
-FROM Employees;
+FROM dbo.Employees;
 go
 
 CREATE
 VIEW
-InventoriesView AS
+vInventories
+WITH SCHEMABINDING
+AS
 SELECT
 InventoryID,InventoryDate,EmployeeID,ProductID,Count
-FROM Inventories;
+FROM dbo.Inventories;
 go
 
 CREATE
 VIEW
-ProductsView AS
+vProducts 
+WITH SCHEMABINDING
+AS
 SELECT
 ProductID,ProductName,CategoryID,UnitPrice
-FROM Products;
+FROM dbo.Products;
 go
 
 
 -- Question 2 (5% pts): How can you set permissions, so that the public group CANNOT select data 
 -- from each table, but can select data from each view?
+Use Assignment06DB_NThibault;
+DENY SELECT ON Categories to Public;
+DENY SELECT ON Employees to Public;
+DENY SELECT ON Products to Public;
+DENY SELECT ON Inventories to Public;
 
+GRANT SELECT ON vCategories to Public;
+GRANT SELECT ON vEmployees to Public;
+GRANT SELECT ON vProducts to Public;
+GRANT SELECT ON vInventories to Public;
+go
 
 -- Question 3 (10% pts): How can you create a view to show a list of Category and Product names, 
 -- and the price of each product?
 -- Order the result by the Category and Product!
+CREATE
+VIEW
+vProductsByCategories AS
+SELECT
+vCategories.CategoryName,
+vProducts.ProductName,
+vProducts.UnitPrice
+FROM vProducts 
+JOIN vCategories
+ON vProducts.CategoryID = vCategories.CategoryID;
+go
 
+SELECT CategoryName, ProductName, UnitPrice FROM vProductsByCategories ORDER BY CategoryName ASC, ProductName ASC;
+go
 
 -- Question 4 (10% pts): How can you create a view to show a list of Product names 
 -- and Inventory Counts on each Inventory Date?
 -- Order the results by the Product, Date, and Count!
+CREATE
+VIEW
+vInventoriesByProductsByDates AS
+SELECT
+vProducts.ProductName,
+vInventories.Count,
+vInventories.InventoryDate
+FROM vInventories
+JOIN vProducts
+ON vInventories.ProductID = vProducts.ProductID;
+go
 
+SELECT ProductName, InventoryDate, Count FROM vInventoriesByProductsByDates ORDER BY ProductName ASC, InventoryDate ASC, Count ASC;
+go
 
 -- Question 5 (10% pts): How can you create a view to show a list of Inventory Dates 
 -- and the Employee that took the count?
@@ -235,29 +278,186 @@ go
 -- 2017-02-01	    Robert King
 -- 2017-03-01	    Anne Dodsworth
 
+CREATE
+VIEW
+vInventoriesByEmployeesByDates
+AS
+SELECT
+	vInventories.InventoryDate,
+	CONCAT(vEmployees.EmployeeFirstName,' ',vEmployees.EmployeeLastName) AS EmployeeName
+FROM vInventories JOIN vEmployees
+ON vEmployees.EmployeeID = vInventories.EmployeeID
+go
+
+SELECT 
+	InventoryDate,
+	EmployeeName
+FROM vInventoriesByEmployeesByDates 
+GROUP BY InventoryDate, EmployeeName;
+go
+
 -- Question 6 (10% pts): How can you create a view show a list of Categories, Products, 
 -- and the Inventory Date and Count of each product?
 -- Order the results by the Category, Product, Date, and Count!
+CREATE
+VIEW
+vInventoriesByProductsByCategories
+AS
+SELECT 
+	vCategories.CategoryName,
+	vProducts.ProductName,
+	vInventories.InventoryDate,
+	vInventories.Count
+FROM vInventories 
+ JOIN vProducts
+  ON vProducts.ProductID = vInventories.ProductID
+ JOIN vCategories
+  ON vProducts.CategoryID = vCategories.CategoryID
+go
 
+SELECT 
+	CategoryName,
+	ProductName,
+	InventoryDate,
+	Count
+FROM vInventoriesByProductsByCategories
+ORDER BY CategoryName ASC, ProductName ASC, InventoryDate ASC,Count ASC;
+go
 
 -- Question 7 (10% pts): How can you create a view to show a list of Categories, Products, 
 -- the Inventory Date and Count of each product, and the EMPLOYEE who took the count?
 -- Order the results by the Inventory Date, Category, Product and Employee!
+CREATE
+VIEW
+vInventoriesByProductsByEmployees
+AS
+SELECT 
+	vCategories.CategoryName,
+	vProducts.ProductName,
+	vInventories.InventoryDate,
+	vInventories.Count,
+	CONCAT(EmployeeFirstName,' ',EmployeeLastName) AS EmployeeName
+FROM vInventories 
+ JOIN vProducts
+  ON vProducts.ProductID = vInventories.ProductID
+ JOIN vCategories
+  ON vProducts.CategoryID = vCategories.CategoryID
+ JOIN vEmployees
+  ON vInventories.EmployeeID = vEmployees.EmployeeID;
+go
 
+SELECT 
+	CategoryName,
+	ProductName,
+	InventoryDate,
+	Count,
+	EmployeeName
+FROM vInventoriesByProductsByEmployees
+ORDER BY InventoryDate, CategoryName, ProductName, EmployeeName;
+go
 
 -- Question 8 (10% pts): How can you create a view to show a list of Categories, Products, 
 -- the Inventory Date and Count of each product, and the Employee who took the count
 -- for the Products 'Chai' and 'Chang'? 
+CREATE
+VIEW
+vInventoriesForChaiAndChangByEmployees
+AS
+SELECT 
+	vCategories.CategoryName,
+	vProducts.ProductName,
+	vInventories.InventoryDate,
+	vInventories.Count,
+	CONCAT(EmployeeFirstName,' ',EmployeeLastName) AS EmployeeName
+FROM vInventories 
+ JOIN vProducts
+  ON vProducts.ProductID = vInventories.ProductID
+ JOIN vCategories
+  ON vProducts.CategoryID = vCategories.CategoryID
+ JOIN vEmployees
+  ON vInventories.EmployeeID = vEmployees.EmployeeID
+WHERE vProducts.ProductID IN (SELECT ProductID FROM vProducts Where (ProductName = 'Chai' OR ProductName = 'Chang'));
+go
+
+Select 
+	CategoryName,
+	ProductName,
+	InventoryDate,
+	Count,
+	EmployeeName
+FROM vInventoriesForChaiAndChangByEmployees
+ORDER BY InventoryDate ASC, CategoryName ASC, ProductName ASC;
+go
 
 
 -- Question 9 (10% pts): How can you create a view to show a list of Employees and the Manager who manages them?
 -- Order the results by the Manager's name!
+CREATE
+VIEW
+vEmployeesByManager
+AS
+SELECT 
+	CONCAT(emp.EmployeeFirstName,' ',emp.EmployeeLastName) AS EmployeeName, 
+	CONCAT(mgr.EmployeeFirstName,' ',mgr.EmployeeLastName) AS ManagerName
+FROM vEmployees AS emp
+JOIN vEmployees mgr
+ON emp.ManagerID = mgr.EmployeeID
+go
 
+SELECT
+	ManagerName,
+	EmployeeName
+FROM
+vEmployeesByManager
+ORDER BY ManagerName ASC;
+go
 
 -- Question 10 (20% pts): How can you create one view to show all the data from all four 
 -- BASIC Views? Also show the Employee's Manager Name and order the data by 
 -- Category, Product, InventoryID, and Employee.
+CREATE
+VIEW
+vInventoriesByProductsByCategoriesByEmployees
+AS
+SELECT
+	vCategories.CategoryID,
+	vCategories.CategoryName,
+	vProducts.ProductID,
+	vProducts.ProductName,
+	vProducts.UnitPrice,
+	vInventories.InventoryID,
+	vInventories.InventoryDate,
+	vInventories.Count,
+	emp.EmployeeID,
+	CONCAT(emp.EmployeeFirstName,' ',emp.EmployeeLastName) AS EmployeeName, 
+	CONCAT(mgr.EmployeeFirstName,' ',mgr.EmployeeLastName) AS ManagerName
+FROM vInventories 
+ JOIN vProducts
+  ON vProducts.ProductID = vInventories.ProductID
+ JOIN vCategories
+  ON vProducts.CategoryID = vCategories.CategoryID
+ JOIN vEmployees emp
+  ON vInventories.EmployeeID = emp.EmployeeID
+ JOIN vEmployees mgr
+  ON emp.ManagerID = mgr.EmployeeID
+go
 
+SELECT
+	CategoryID,
+	CategoryName,
+	ProductID,
+	ProductName,
+	UnitPrice,
+	InventoryID,
+	InventoryDate,
+	Count,
+	EmployeeID,
+	EmployeeName, 
+	ManagerName
+FROM
+vInventoriesByProductsByCategoriesByEmployees
+ORDER BY CategoryName, ProductName, InventoryID, EmployeeName
+go
 
 -- Test your Views (NOTE: You must change the your view names to match what I have below!)
 Print 'Note: You will get an error until the views are created!'
